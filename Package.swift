715273534584e48db01773c3669fee63f27ea487
Nunu Nugraha <nununugraha@Nunus-MacBook-Pro.local>
2025-11-04 10:45:07 +0700

    update dependency to meals appcore

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.47.0")),
         // 2. Konsisten dengan versi stabil Alamofire (dari 5.9.1)
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
-        .package(url: "https://github.com/nunutech40/MealsAppCore.git", from: "1.0.0"),
+        .package(url: "https://github.com/nunutech40/MealsAppCore.git", from: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
